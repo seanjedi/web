7d3ef3c26536bbc481379fd108f970da9cc63ba5
Sean Patrick Malloy <spmalloy@ucdavis.edu>
2018-03-14 15:32:10 -0700

    new format

---
+++ web_code/login.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Login</title>
+    </head>
+    <body>
+    <?php
+        $usersName = $_POST['username'];
+        $passWord = $_POST['password'];
+        echo $usernames;
+    ?>
+    </body>
+</html>
